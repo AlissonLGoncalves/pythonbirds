aaec7f70e97f370f6fa3f8b41c11a61e8288703c
AlissonL Gonçalves <alisson.16lg@gmail.com>
2022-03-04 20:40:56 -0300

    Criado atributo complexo 'filhos'

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Luiz')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Alisson'
-    print(p.nome)
-    print(p.idade)
+    alisson = Pessoa(nome='Alisson')
+    edivaldo = Pessoa(alisson, nome='Edivaldo')
+    print(Pessoa.cumprimentar(edivaldo))
+    print(id(edivaldo))
+    print(edivaldo.cumprimentar())
+    print(edivaldo.nome)
+    print(edivaldo.idade)
+    for filho in edivaldo.filhos:
+        print(filho.nome)
